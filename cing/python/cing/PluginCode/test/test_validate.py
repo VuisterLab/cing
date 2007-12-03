65c9709649cbae25391477aaca9def4a9ecd7f49
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2007-12-03 12:01:51 +0000

    Removed some spurious test data.

--- cing/python/cing/PluginCode/test/test_validate.py
+++ cing/python/cing/PluginCode/test/test_validate.py
@@ -2,7 +2,6 @@
 Unit test
 python $cingPath/PluginCode/test/test_Wattos.py
 """
-from cing import cingRoot
 from cing.Libs.NTutils import SetupError
 from cing.core.classes import Project
 from unittest import TestCase
@@ -16,16 +15,12 @@ class AllChecks(TestCase):
     def testrun(self):
         """wattos run check"""
 #        SETUP FIRST
-        dirTestsData        = os.path.join(cingRoot, "Tests", "data" )         
         if os.chdir(cingDirTestsTmp):
             raise SetupError("Failed to change to directory for temporary test files: "+cingDirTestsTmp)
 
         entryId = "1brv" # Small much studied PDB NMR entry 
         pdbFileName = entryId+"_small.pdb"
         pdbFilePath = os.path.join( cingDirTestsData, pdbFileName)
-        check_FileName = entryId + "_check_small.db"
-        check_FilePath = os.path.join( dirTestsData, check_FileName )
-        self.assertTrue(os.path.exists(check_FilePath))
  
         
         if os.chdir(cingDirTestsTmp):
