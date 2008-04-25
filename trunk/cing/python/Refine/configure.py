601309d7484a2f8e119065fbd726f56f93e1d995
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2008-04-25 12:28:50 +0000

    Reinitialized from Geerten's code send by pigeon.

---
+++ trunk/cing/python/Refine/configure.py
@@ -0,0 +1,54 @@
+# **********************************************************
+# *                                                        *
+# *       configure.py                                     *
+# *                                                        *
+# **********************************************************
+# *   Adapted as configure.py by GWV Feb 2005/March 2007   *
+# **********************************************************
+#
+# This configuration file contains all necessary information 
+# for the refine.py script.
+
+from cing.Libs.NTutils import NTstruct
+import os
+
+refinePath = os.path.join(os.getenv('CINGROOT','.'), 'python','Refine')
+xplorPath  = os.getenv('xplorPath','.')
+
+# **********************************************************
+# editing below here should generally not be necessary
+# **********************************************************
+# Relabelled from config to refineConfig for apparent clashes with other config.
+config = NTstruct(  
+
+    XPLOR            =  xplorPath,
+
+# (waterrefine) xplor stuff, directories, etc.
+    refinePath       = refinePath,
+    topparPath       = os.path.join(refinePath,'toppar'), 
+    protocolsPath    = os.path.join(refinePath,'explicit_solvent'), 
+
+    parameterFiles   = [ 'parallhdg5.3-merge.pro',
+                         'parallhdg5.3.sol',
+                         'par_axis_aria.pro',
+                         'ion.param',
+                       ],
+    topologyFiles    = [ 'topallhdg5.3-merge.pro',
+                         'topallhdg5.3.sol',
+                         'top_axis_aria.pro',
+                         'ion.top',
+                       ],
+    directories      = NTstruct(
+        converted    = 'Converted',
+        analyzed     = 'Analyzed',
+        refined      = 'Refined', 
+        tables       = 'Tables',
+        jobs         = 'Jobs', 
+        psf          = 'PSF',
+	toppar	     = 'Toppar'
+    )
+)
+
+config.directories.keysformat()
+config.keysformat()
+#print '>>', config.format()
