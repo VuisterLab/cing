61f1910053aef0f4bf3ece49e7581608fe5d4cb5
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2008-05-08 19:20:03 +0000

    *** empty log message ***

--- trunk/cing/python/cing/Libs/cython/compile.py
+++ trunk/cing/python/cing/Libs/cython/compile.py
@@ -1,14 +1,14 @@
 # usage: python compile.py build_ext --inplace
 # TODO automate this from CING setup script.
 from distutils.core import setup
-from Cython.Distutils.extension import Extension
+#from Cython.Distutils.extension import Extension # absent in fink install of cython-py25_0.9.6.10b-1_darwin-i386.deb
+from distutils.extension import Extension # absent in fink install of cython-py25_0.9.6.10b-1_darwin-i386.deb
 from Cython.Distutils.build_ext import build_ext
 
 
 setup(
   name = 'Superpose',
   ext_modules=[ 
-#    Extension("vector",       ["vector.pyx"]),
     Extension("superpose",       ["superpose.pyx"])
     ],
   cmdclass = {'build_ext': build_ext}
