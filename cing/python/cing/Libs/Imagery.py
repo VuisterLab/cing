aa8ff32fb759bbbf356a98526db9a6110ab1e5f6
alanwilter <alanwilter@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2008-07-17 16:54:12 +0000

    AWSS: convert and montage work better with pdf than ps; e.g. montaged figs are not trimmed from 2nd page and on as it happens when input is ps. And it also works with defectives imagemagick distributions in e.g. Ubuntu and Fink for Tiger.

--- cing/python/cing/Libs/Imagery.py
+++ cing/python/cing/Libs/Imagery.py
@@ -30,8 +30,8 @@ def montageImageMagick(inputPath,outputPath,options,extraOptions=None):
 
 def convertGhostScript(inputPath,options,extraOptions=None):
     # GS has rather verbose output
-    # just to make sure we report back on on error let's have caller check results. 
-    gs = ExecuteProgram(cingPaths.ghostscript, redirectOutputToFile='/dev/null') 
+    # just to make sure we report back on on error let's have caller check results.
+    gs = ExecuteProgram(cingPaths.ghostscript, redirectOutputToFile='/dev/null')
     cmd = options
     if extraOptions:
         cmd += " " + extraOptions
@@ -49,15 +49,15 @@ def convertPs2Pdf(inputPath,outputPath,options,extraOptions=None):
     if convert( cmd ):
         NTerror("Failed to run conversion: " + cmd)
         return True
-         
+
 def joinPdfPagesByConvert( inputFileList, outputPath):
     """Rasterizes which reduces the quality in general"""
-    # convert -adjoin -delay 200 residuePlotSet001.png residuePlotSet002.png residuePlotSetAll.gif    
+    # convert -adjoin -delay 200 residuePlotSet001.png residuePlotSet002.png residuePlotSetAll.gif
     options = '-adjoin -delay 200'
     if convertImageMagick(' '.join(inputFileList), outputPath, options):
         NTerror("Failed to joinPdfPagesByConvert to output: " + outputPath)
         return True
-    
+
 def joinPdfPagesByGhostScript( inputFileList, outputPath):
     # gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=Merged.pdf -dBATCH residuePlotSet000.pdf residuePlotSet001.pdf
     options = '-dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=%s -dBATCH ' % \
@@ -66,22 +66,22 @@ def joinPdfPagesByGhostScript( inputFileList, outputPath):
         NTerror("Failed to joinPdfPagesByGhostScript to output: " + outputPath)
         return True
     if not os.path.isfile(outputPath):
-        # just to make sure we report back on on error let's have caller check results. 
+        # just to make sure we report back on on error let's have caller check results.
         NTerror("Failed joinPdfPagesByGhostScript because no output: " + outputPath)
         return True
 # Perhaps configurable to convert
 #joinPdfPages = joinPdfPagesByConvert
 joinPdfPages = joinPdfPagesByGhostScript
-    
+
 def convert2Web(path, outputDir=None, doFull=True, doPrint=True, doMontage=False):
     """Using the system convert from ImageMagick several pieces of imagery will be created:
         a- pinup (smallish gif usable as an preview; 100 width by 1xx for A4 aspect ratio)
-        b- full size 1(gif of 1000 width or montaged png of 1000 width for each page) 
+        b- full size 1(gif of 1000 width or montaged png of 1000 width for each page)
         c- printable version (pdf)
 
        The output file names are automatically generated. If the outputDir is set it will be
        used. If not set, the same directory as the input will be used.
-       
+
        Returns None for error and list of output files otherwise. A None in the list means\
        the plot was not generated.
        Gif files are multipaged with 2 second intervals.
@@ -89,11 +89,11 @@ def convert2Web(path, outputDir=None, doFull=True, doPrint=True, doMontage=False
 
        The input path can be with or without directory and can be
        an absolute or relative path. It may also be a list of images.
-       
-       If the input is a list then the output file names will be tried to shorten by the 
+
+       If the input is a list then the output file names will be tried to shorten by the
        "%03d" formatting assumed if possible.
        E.g. residuePlotSetAll001.gif as the first filename will give a:
-            residuePlotSetAll.gif and residuePlotSetAll_pin.gif output.  
+            residuePlotSetAll.gif and residuePlotSetAll_pin.gif output.
     """
     optionsPinUp = "-delay 200 -geometry 102" # geometry's first argument is width
     optionsFull  = "-delay 200 -geometry 1024"
@@ -108,14 +108,14 @@ def convert2Web(path, outputDir=None, doFull=True, doPrint=True, doMontage=False
                 return True
         pathStr = ' '.join(path)
         pathFirst = path[0]
-            
+
     else:
         if not os.path.exists(path):
             NTerror("Failed to find input: " + path)
             return True
         pathStr = path
-        pathFirst = pathStr 
-        
+        pathFirst = pathStr
+
     # Next time use: NTpath for this.
 #    path = "/Users/jd/t.pdf"
     head, tail = os.path.split(pathFirst)             # split is on last /
@@ -123,7 +123,7 @@ def convert2Web(path, outputDir=None, doFull=True, doPrint=True, doMontage=False
 
     if isinstance(path, list):
         root = removeTrailingNumbers( root )
-        
+
     if outputDir:
         if os.path.exists(outputDir) and os.path.isdir(outputDir):
             head = outputDir
@@ -131,19 +131,33 @@ def convert2Web(path, outputDir=None, doFull=True, doPrint=True, doMontage=False
             NTerror("Given output directory: " + outputDir + " is absent or is not a dir")
             return None
 
-    if extension == "pdf":
+    if extension == ".pdf":
         NTdebug("Will skip generating printable version as input is also a pdf")
         doPrint = False
 
-    if extension == "gif":
+    if extension == ".gif":
         NTdebug("Will skip generating full size gif version as input is also a gif")
         doFull = False
 
     pinupPath = None
     fullPath  = None
     printPath = None
+
+    # AWSS: convert and montage work better with pdf than ps
+    # e.g. montaged figs are not trimmed from 2nd page and on as it happens
+    # when input is ps.
+    if extension == ".ps":
+        doPrint = True
+
     # Algorithm below can be speeded up by not rereading the input but scripting the
     #     generation of 3 outputs.
+    if doPrint:
+        printPath = os.path.join( head, root+".pdf")
+        if convertPs2Pdf( pathStr, printPath, optionsPrint):
+            NTerror("Failed to generated print")
+            printPath = None
+        pathStr = pathFirst = printPath
+
     if doPinUp:
         pinupPath = os.path.join( head, root+"_pin.gif")
         if convertImageMagick(pathStr, pinupPath, optionsPinUp):
@@ -160,11 +174,7 @@ def convert2Web(path, outputDir=None, doFull=True, doPrint=True, doMontage=False
             if convertImageMagick(pathStr, fullPath, optionsFull):
                 NTerror("Failed to generated full gif")
                 fullPath = None
-    if doPrint:
-        printPath = os.path.join( head, root+".pdf")
-        if convertPs2Pdf( pathStr, printPath, optionsPrint):
-            NTerror("Failed to generated print")
-            printPath = None
+
     result = ( pinupPath, fullPath, printPath )
     if  pinupPath or fullPath or printPath:
         return result
@@ -172,13 +182,13 @@ def convert2Web(path, outputDir=None, doFull=True, doPrint=True, doMontage=False
 
 def removeTrailingNumbers(fileName):
     p = re.compile( '\d+$' ) # I didn't get this to work with string.replace()
-    return p.sub('', fileName)  
+    return p.sub('', fileName)
 
 def montage(pathList, outputFileName, extraOptions = None ):
     """Using the system montage from ImageMagick an html file will piece together the given
         imagery in full size:
         a- full size 1(gif of original size)
-       
+
        Returns None for success.
        Input can be anything ImageMagick reads, e.g. Postscript produced by Procheck_NMR.
        The input pathList may also be a single file such as a multiple page .ps.
@@ -190,11 +200,11 @@ def montage(pathList, outputFileName, extraOptions = None ):
 #    optionsFull +=  backgroundColor
     if extraOptions:
         optionsFull += ' ' + extraOptions
-    
+
     if isinstance(pathList, list):
         pathStr = ' '.join(pathList)
     else:
         pathStr = pathList
-        
+
     if montageImageMagick(pathStr, outputFileName, optionsFull):
         NTerror("Failed to generated full html")
