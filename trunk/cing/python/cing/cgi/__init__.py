9e22656a26673f2f57f8b6ab10fc40c82cf696dc
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2008-10-21 18:07:13 +0000

    Settling on two-part iCing server side now. CGI for file uploads but a fast regular server for all other.

--- trunk/cing/python/cing/cgi/__init__.py
+++
@@ -1,8 +0,0 @@
-#-----------------------------------------------------------------------------
-#
-# File to allow this directory to be treated as a python package.
-# 
-#-----------------------------------------------------------------------------
-
-
-# FileUpload.py itself doesn't know it's in a package on the production server.
