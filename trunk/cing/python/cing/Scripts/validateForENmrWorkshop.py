84b01fc4a40a54ca4da0565536848e9fae500bf0
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2009-05-01 12:17:52 +0000

    - Adding index to eNMR workshop data.

--- trunk/cing/python/cing/Scripts/validateForENmrWorkshop.py
+++ trunk/cing/python/cing/Scripts/validateForENmrWorkshop.py
@@ -32,7 +32,7 @@ doScriptOnEntryList(pythonScriptFileName,
                     entryListFileName,
                     startDir,
                     processes_max = 2,
-                    delay_between_submitting_jobs = 15, # why is this so long? because of time outs at tang?
+                    delay_between_submitting_jobs = 5, # why is this so long? because of time outs at tang?
                     max_time_to_wait = 3600, # 1y4o took more than 600. This is one of the optional arguments.
                     # 1ai0 took over 20 min; let's set this to 1 hour
                     START_ENTRY_ID = 0,
