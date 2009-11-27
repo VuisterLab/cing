0d8d3597e73fdff7b6da2c53344e965ed3189384
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2009-11-27 10:13:00 +0000

    Disabling Shiftx on 64bit Linux

--- cing/python/cing/PluginCode/shiftx.py
+++ cing/python/cing/PluginCode/shiftx.py
@@ -2,6 +2,8 @@
 Adds shiftx method to predict chemical shifts. The shiftx program is included as binaries for Mac OSX and 32 bit
 Linux in the bin directory.
 """
+from cing import OS_TYPE_LINUX
+from cing import osType
 from cing.Libs.AwkLike import AwkLike
 from cing.Libs.NTutils import ExecuteProgram
 from cing.Libs.NTutils import ImportWarning
@@ -25,6 +27,7 @@ from cing.core.parameters import validationSubDirectories
 from math import sqrt
 import cing
 import os
+import platform
 
 
 if True: # block
@@ -33,6 +36,12 @@ if True: # block
     if not os.path.exists( cingPaths.shiftx ):
         NTdebug("Missing shiftx which is a dep for shiftx")
         useModule = False
+
+    if osType == OS_TYPE_LINUX and platform.architecture()[0] == '64bit':
+        NTdebug("Included shiftx fails on 64bit Linux so disabling the plugin.")
+        raise ImportWarning('shiftx')
+
+
     if not useModule:
         raise ImportWarning('shiftx')
 #    NTmessage('Using shiftx')
