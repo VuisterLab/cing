1b66635629ca911d2b19115026132f9626477dc8
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2010-04-30 21:55:32 +0000

    - Split extra resources in index into lists and added pgsql resource.

--- cing/python/cing/NRG/sql/dumpNRG-CING.sql
+++ cing/python/cing/NRG/sql/dumpNRG-CING.sql
@@ -1,3 +1,4 @@
-COPY (SELECT * FROM entry) TO '/Users/jd/tmp/pgsql/entry.csv' WITH CSV HEADER;
-COPY (SELECT * FROM chain) TO '/Users/jd/tmp/pgsql/chain.csv' WITH CSV HEADER;
-COPY (SELECT * FROM residue) TO '/Users/jd/tmp/pgsql/residue.csv' WITH CSV HEADER;
+COPY (SELECT * FROM entry) TO '/Users/jd/CASD-NMR-CING/pgsql/entry.csv' WITH CSV HEADER;
+COPY (SELECT * FROM chain) TO '/Users/jd/CASD-NMR-CING/pgsql/chain.csv' WITH CSV HEADER;
+COPY (SELECT * FROM residue) TO '/Users/jd/CASD-NMR-CING/pgsql/residue.csv' WITH CSV HEADER;
+COPY (SELECT * FROM atom) TO '/Users/jd/CASD-NMR-CING/pgsql/atom.csv' WITH CSV HEADER;
