cc77f2b23995373d1a39deb5cc8744e8a123d122
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2010-12-14 12:25:48 +0000

    See previous commit.

---
+++ cing/python/cing/NRG/test/test_BMRBcounts.py
@@ -0,0 +1,74 @@
+"""
+Unit test execute as:
+python $CINGROOT/python/cing/NRG/test/test_BMRBcounts.py
+"""
+from cing import cingDirData
+from cing import cingDirTestsData
+from cing import cingDirTmp
+from cing.Libs.DBMS import DBMS
+from cing.Libs.NTutils import * #@UnusedWildImport
+from cing.NRG.PDBEntryLists import getBmrbCsCounts
+from cing.core.classes import Project
+from unittest import TestCase
+import unittest
+
+class AllChecks(TestCase):
+
+    def _testCounts(self):
+
+        self.failIf(os.chdir(cingDirTmp), msg =
+            "Failed to change to directory for temporary test files: " + cingDirTmp)
+        dbms = DBMS()
+        bmrbCountTableName = 'BMRB_CS_counts'
+        dbms.readCsvRelationList([bmrbCountTableName], os.path.join(cingDirData, 'NRG'))
+        bmrbCountTable = dbms.tables[ bmrbCountTableName ]
+        bmrbCountTable.convertColumn(0) # default is integer data type converting the read strings
+        bmrbCountTable.convertColumn(2)
+        bmrbCountTableProper = bmrbCountTable.toTable()
+#        NTdebug("Found table: %r" % bmrbCountTableProper)
+        bmrbCountMap = NTdict()
+#        idxColumnKeyList = [0, 1, 2]
+        idxColumnKeyList = []
+        bmrbCountMap.appendFromTableGeneric(bmrbCountTableProper, *idxColumnKeyList)
+        bmrbId = 4020
+        self.assertEqual( getDeepByKeysOrAttributes( bmrbCountMap, bmrbId, '1H' ), 183)
+        self.assertEqual( getDeepByKeysOrAttributes( bmrbCountMap, bmrbId, '13C' ), 73)
+        self.assertEqual( getDeepByKeysOrAttributes( bmrbCountMap, bmrbId, '15N' ), None)
+
+    def testCount2(self):
+
+        self.failIf(os.chdir(cingDirTmp), msg =
+            "Failed to change to directory for temporary test files: " + cingDirTmp)
+
+        entryId = '1brv_cs_pk_2mdl'
+        bmrbId = 4020
+        inputArchiveDir = os.path.join(cingDirTestsData, "ccpn")
+        ccpnFile = os.path.join(inputArchiveDir, entryId + ".tgz")
+        project = Project.open(entryId, status = 'new')
+        self.assertTrue(project.initCcpn(ccpnFolder = ccpnFile))
+        assignmentCountMap = project.molecule.getAssignmentCountMap()
+#        p_cs_count = assignmentCountMap['overall']
+        p_cs1H_count = assignmentCountMap['1H']
+        p_cs13C_count = assignmentCountMap['13C']
+        p_cs15N_count = assignmentCountMap['15N']
+
+        bmrbCountMap = getBmrbCsCounts()
+        NTdebug("bmrbCountMap %r" % bmrbCountMap)
+        entryMap = getDeepByKeysOrAttributes( bmrbCountMap, bmrbId )
+        NTdebug("entryMap %r" % entryMap)
+        d_cs1H_count = getDeepByKeysOrAttributes( entryMap, '1H' )
+        d_cs13C_count = getDeepByKeysOrAttributes( entryMap, '13C' )
+        d_cs15N_count = getDeepByKeysOrAttributes( entryMap, '15N' )
+
+        NTdebug("db: %s project: %s" % ( d_cs1H_count, p_cs1H_count ) )
+        NTdebug("db: %s project: %s" % ( d_cs13C_count, p_cs13C_count ) )
+        NTdebug("db: %s project: %s" % ( d_cs15N_count, p_cs15N_count ) )
+
+#        self.assertTrue( d_cs1H_count, p_cs1H_count) # was 183
+#        self.assertTrue( d_cs13C_count, p_cs13C_count) # was 73
+#        self.assertTrue( d_cs15N_count, p_cs15N_count)
+
+
+if __name__ == "__main__":
+    cing.verbosity = verbosityDebug
+    unittest.main()
