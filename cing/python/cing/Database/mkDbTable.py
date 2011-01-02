e32f50210513390c3acead0bd8d696f0bb37398a
g.vuister@gmail.com <g.vuister@gmail.com@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2011-01-02 19:30:56 +0000

    Complete commit

--- cing/python/cing/Database/mkDbTable.py
+++
@@ -1,9 +0,0 @@
-#@PydevCodeAnalysisIgnore
-from cing.Libs.NTutils import *
-from cing.core.database import ResidueDef
-from cing import NTdb
-import cing
-
-stream = open('dbTable.new', 'w')
-NTdb.exportDef(stream=stream, convention='INTERNAL_1')
-stream.close()
