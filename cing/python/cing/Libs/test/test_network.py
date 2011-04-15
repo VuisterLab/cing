d2909f7a350c9fc269d814f2a1752652055daaed
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2011-04-15 21:28:31 +0000

    - Setting up VC settings with new target.

---
+++ cing/python/cing/Libs/test/test_network.py
@@ -0,0 +1,18 @@
+from cing import cingDirTestsData
+from cing.Libs.NTutils import * #@UnusedWildImport
+from cing.Libs.network import sendFileByScp
+from unittest import TestCase
+import unittest
+
+class AllChecks(TestCase):
+
+    def _test_sendFileByScp(self):
+        entryId = '1brv'
+        fileName = os.path.join(cingDirTestsData, "ccpn", entryId + ".tgz")
+        targetUrl = 'jd@dodos.dyndns.org:/Users/jd/tmp/x/y/z/a/b'
+        NTdebug("Trying targetUrl: %s" % targetUrl)
+        self.assertFalse( sendFileByScp( fileName, targetUrl ))
+
+if __name__ == "__main__":
+    cing.verbosity = verbosityDebug
+    unittest.main()
