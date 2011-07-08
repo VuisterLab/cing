97faf942154479c44a14317f99ade144227886fe
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2011-07-08 11:05:24 +0000

    - Unfortunately redoing code duplication because of bug.

--- trunk/cing/python/cing/setupCing.py
+++ trunk/cing/python/cing/setupCing.py
@@ -22,16 +22,47 @@ respective functionalities.
 
 # Please note the lack of imports here to cing specific code.
 # The idea is that this script runs without PYTHONPATH being set yet.
-from cing.Libs.helper2 import _NTerror
-from cing.Libs.helper2 import _NTgetoutput
-from cing.Libs.helper2 import _NTmessage
-from cing.Libs.helper2 import _NTwarning
 from string import atoi
 from string import strip
+from subprocess import PIPE
+from subprocess import Popen
 import os
 import sys
-import time
 #import warnings
+
+#-----------------------------------------------------------------------------------
+# Synchronize block with cing.Libs.helper.py
+#-----------------------------------------------------------------------------------
+def _NTgetoutput( cmd ):
+    """Return output from command as (stdout,sterr) tuple"""
+#    inp,out,err = os.popen3( cmd )
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+    (inpStream,outStream,errStream) = (p.stdin, p.stdout, p.stderr)
+
+    output = ''
+    for line in outStream.readlines():
+        output += line
+    errors = ''
+    for line in errStream.readlines():
+        errors += line
+    inpStream.close()
+    outStream.close()
+    errStream.close()
+    return (output,errors)
+# end def
+def _NTerror(msg):
+    'Convenience method'
+    print "ERROR:",msg
+# end def
+def _NTwarning(msg):
+    'Convenience method'
+    print "WARNING:",msg
+# end def
+def _NTmessage(msg):
+    'Convenience method'
+    print msg
+# end def
+#-----------------------------------------------------------------------------------
 
 # disabling some output that can safely be ignored
 #warnings.simplefilter('ignore', category=UserWarning)
