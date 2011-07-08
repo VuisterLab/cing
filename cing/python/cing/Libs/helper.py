cacb315f8ac95e766a843180aad109bef5af592e
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2011-07-08 11:05:24 +0000

    - Unfortunately redoing code duplication because of bug.

--- cing/python/cing/Libs/helper.py
+++ cing/python/cing/Libs/helper.py
@@ -2,13 +2,47 @@
 Very simple functions only here that can be instantiated without the general CING setup.
 Called from cing's main __init__.py and setupCing.py.
 """
-from cing.Libs.helper2 import _NTgetoutput
+from subprocess import PIPE
+from subprocess import Popen
 import os
 import platform
 import sys
 import time
 import urllib2
 
+#-----------------------------------------------------------------------------------
+# Synchronize block with cing.Libs.helper.py
+#-----------------------------------------------------------------------------------
+def _NTgetoutput( cmd ):
+    """Return output from command as (stdout,sterr) tuple"""
+#    inp,out,err = os.popen3( cmd )
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+    (inpStream,outStream,errStream) = (p.stdin, p.stdout, p.stderr)
+
+    output = ''
+    for line in outStream.readlines():
+        output += line
+    errors = ''
+    for line in errStream.readlines():
+        errors += line
+    inpStream.close()
+    outStream.close()
+    errStream.close()
+    return (output,errors)
+# end def
+def _NTerror(msg):
+    'Convenience method'
+    print "ERROR:",msg
+# end def
+def _NTwarning(msg):
+    'Convenience method'
+    print "WARNING:",msg
+# end def
+def _NTmessage(msg):
+    'Convenience method'
+    print msg
+# end def
+#-----------------------------------------------------------------------------------
 
 def getSvnRevision():
     """Return the revision number (int) or None if the revision isn't known. It depends on svn being available on the system."""
