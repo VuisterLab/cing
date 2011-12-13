a75923bb3d7fde6aa663b72d9736a1c82a9a6e02
jurgenfd <jurgenfd@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2011-12-13 15:20:50 +0000

    - Debugging optional dependency checking.

--- cing/python/cing/PluginCode/test/test_Analysis.py
+++ cing/python/cing/PluginCode/test/test_Analysis.py
@@ -8,10 +8,6 @@ from cing import cingDirTmp
 from cing.Libs.NTutils import * #@UnusedWildImport
 from cing.PluginCode.required.reqAnalysis import ANALYSIS_STR
 from cing.PluginCode.required.reqCcpn import CCPN_STR
-from cing.Scripts.Analysis.PyRPF import DEFAULT_CONSIDER_ALIASED_POSITIONS
-from cing.Scripts.Analysis.PyRPF import DEFAULT_DIAGONAL_EXCLUSION_SHIFT
-from cing.Scripts.Analysis.PyRPF import DEFAULT_DISTANCE_THRESHOLD
-from cing.Scripts.Analysis.PyRPF import DEFAULT_PROCHIRAL_EXCLUSION_SHIFT
 from cing.core.classes import Project
 from nose.plugins.skip import SkipTest
 from unittest import TestCase
@@ -26,6 +22,10 @@ except ImportWarning, extraInfo: # Disable after done debugging; can't use nTdeb
 # end try
 try:
     from cing.PluginCode.Analysis import Analysis #@UnusedImport needed to throw a ImportWarning so that the test is handled properly.
+    from cing.Scripts.Analysis.PyRPF import DEFAULT_CONSIDER_ALIASED_POSITIONS
+    from cing.Scripts.Analysis.PyRPF import DEFAULT_DIAGONAL_EXCLUSION_SHIFT
+    from cing.Scripts.Analysis.PyRPF import DEFAULT_DISTANCE_THRESHOLD
+    from cing.Scripts.Analysis.PyRPF import DEFAULT_PROCHIRAL_EXCLUSION_SHIFT
 except ImportWarning, extraInfo: # Disable after done debugging; can't use nTdebug yet.
     print "Got ImportWarning %-10s Skipping unit check %s." % ( ANALYSIS_STR, getCallerFileName() )
     raise SkipTest(ANALYSIS_STR)
