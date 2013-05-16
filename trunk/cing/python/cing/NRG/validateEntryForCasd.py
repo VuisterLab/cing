8056a893c80e3682a681e515a9f63d4d0bad2ca9
r.h.fogh <r.h.fogh@e0b8d8eb-cb4c-0410-9204-db9c3ed855ff>
2013-05-16 12:53:55 +0000

    Fixed Typo

--- trunk/cing/python/cing/NRG/validateEntryForCasd.py
+++ trunk/cing/python/cing/NRG/validateEntryForCasd.py
@@ -153,7 +153,7 @@ def mainCasd(entryId, *extraArgList):
             return True
     elif projectType == PROJECT_TYPE_CCPN:
         project = Project.open(entryId, status='new')
-        if not project.initCcpn(ccpnFolder=fileNameTgz, modelCount=modelCount)
+        if not project.initCcpn(ccpnFolder=fileNameTgz, modelCount=modelCount):
                                 #nmrCalcName='CING'):
                                 #nmrCalcName='CASD-NMR'):
             nTerror("Failed to init project from ccpn")
