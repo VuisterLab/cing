fce60d5b91f2b9862b01d82366279d9d1ee6fd16
tjr22@le.ac.uk <username@email.com>
2015-05-11 15:06:21 +0100

    Renamed project.py to Project.py

--- python/cing/core/project.py
+++ python/cing/core/project.py
@@ -1,5 +1,8 @@
 """
 V3 Code to put Project related classes
+Filename starts with capital P in order to preserve 'project' for actual Project instances
+in many routines application code.
+
 Just a stub for now
 """
 
@@ -10,15 +13,17 @@ from cing.core.classes import Project
 from cing.core.classes import History
 from cing.core.classes import StatusDict
 
-from cing.core.constants import PROJECT_OLD
-from cing.core.constants import PROJECT_CREATE
-from cing.core.constants import PROJECT_NEW
-from cing.core.constants import PROJECT_NEWFROMCCPN
-from cing.core.constants import PROJECT_OLDFROMCCPN
-
+from cing.constants import PROJECT_OLD
+from cing.constants import PROJECT_CREATE
+from cing.constants import PROJECT_NEW
+from cing.constants import PROJECT_NEWFROMCCPN
+from cing.constants import PROJECT_OLDFROMCCPN
 
 # convenience methods
 open = Project.open
+
+def old(name, restore=True):
+    return Project.open(name, status=PROJECT_OLD, restore=restore)
 
 def create(name, restore=True):
     return Project.open(name, status=PROJECT_CREATE, restore=restore)
