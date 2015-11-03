d802a9b6b3871bf9dcc341cf4afdb99d82ba116e
TJ Ragan <tjr22@le.ac.uk>
2015-11-03 14:39:15 +0000

    Progress towards functional Django based iCing site...

--- python/cing/vuisterSite/iCing/urls.py
+++ python/cing/vuisterSite/iCing/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.UploadView.as_view(), name='upload'),
-    # url(r'^index$', views.index, name='index'),
+    # url(r'^index$', views.index, name='iCing_index'),
     url(r'^upload$', views.UploadView.as_view(), name='upload'),
     url(r'^options/(?P<submission_code>\w{6})/$', views.OptionsView.as_view(), name='options'),
     url(r'^run/(?P<submission_code>\w{6})$', views.run, name='run'),
