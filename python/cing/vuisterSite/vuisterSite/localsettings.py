e02d3e104e2be62206cecbae9c80c8318aadd0c1
TJ Ragan <tjr22@le.ac.uk>
2015-11-20 10:11:05 +0000

    Untracking log files

--- python/cing/vuisterSite/vuisterSite/localsettings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault('DATABASELOCATION', '88.119.17.144')
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
-os.environ.setdefault('ICING_DATA_DIRECTORY', os.path.join('/','Users', 'tjr22', 'Desktop', 'iCingData'))
-os.environ.setdefault('CINGROOT', os.path.join('/Users', 'tjr22', 'Documents', 'CING', 'cing'))
